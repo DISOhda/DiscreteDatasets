ed167a964729c4ed76fa339a6926b486ad703134
DISOhda <diso.fbmn@h-da.de>
2025-08-21 10:22:25 +0200

    Change order to match the one of previously used excerpt in a paper

--- data-raw/impc2015.R
+++ data-raw/impc2015.R
@@ -13,17 +13,17 @@ IMPC_sel <- IMPC_full %>%
     Male.Mutant.Count,   Male.Control.Count,   Male_AbRateWT, Male_AbRateKO,
   ) %>%
   mutate(
-    Male.Mutant.Atypical    = round(Male_AbRateKO  * Male.Mutant.Count),
-    Male.Mutant.Typical     = Male.Mutant.Count    - Male.Mutant.Atypical,
-    Male.Control.Atypical   = round(Male_AbRateWT  * Male.Control.Count),
-    Male.Control.Typical    = Male.Control.Count   - Male.Control.Atypical,
     Female.Mutant.Atypical  = round(Fem_AbRateKO   * Female.Mutant.Count),
     Female.Mutant.Typical   = Female.Mutant.Count  - Female.Mutant.Atypical,
     Female.Control.Atypical = round(Fem_AbRateWT   * Female.Control.Count),
     Female.Control.Typical  = Female.Control.Count - Female.Control.Atypical,
+    Male.Mutant.Atypical    = round(Male_AbRateKO  * Male.Mutant.Count),
+    Male.Mutant.Typical     = Male.Mutant.Count    - Male.Mutant.Atypical,
+    Male.Control.Atypical   = round(Male_AbRateWT  * Male.Control.Count),
+    Male.Control.Typical    = Male.Control.Count   - Male.Control.Atypical,
     across(ends_with("ypical"), as.integer)
   ) %>%
-  "["(c(1:6, rev(15:22)))
+  "["(c(1:6, 15:22))
 
 attr(IMPC_sel, "problems") <- NULL
 attr(IMPC_sel, "spec")     <- NULL
